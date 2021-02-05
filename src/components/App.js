2004dc75785cef3d3621e7a16de47f9b60ec87ba
wcharlesknight <williamchknight@gmail.com>
2021-02-04 20:36:23 -0800

    done

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,41 @@ class App extends React.Component {
     super()
 
     this.state = {
+   
       pets: [],
       filters: {
         type: 'all'
       }
     }
   }
+
+
+ 
+  changeFilter = (event) => {
+    this.setState({
+      filters:{
+        ...this.state.filters,
+         type: event.target.value
+      }
+    })
+  }
+
+  clickPets = () => {
+           
+    if (this.state.filters.type == 'all'){
+      fetch('/api/pets')
+      .then(res => res.json()).then(pets => this.setState({ pets: pets}))
+    } else {fetch(`/api/pets?type=${this.state.filters.type}`).then(res => res.json()).then(pets => this.setState({ pets: pets}))}
+  }
+
+
+  onAdoptPet = petId => {
+    const pets = this.state.pets.map(p => {
+      return p.id === petId ? { ...p, isAdopted: true } : p;
+    });
+    this.setState({ pets: pets });
+  };
+
 
   render() {
     return (
@@ -24,10 +53,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.changeFilter} onFindPetsClick={this.clickPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets}  onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,25 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
-  render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+  
+  state = {
+    isAdopted: false 
+  }
+
+  isAdopted = () => {
+    this.setState(previous => {
+      return {
+      isAdopted: !previous.isAdopted }
+    })  
+  }
+  
+  render()
+    {
+    return <div className="ui cards"> 
+     
+        {this.props.pets.map(pet => <a><Pet key={pet.id} pet={pet} onAdoptPet={this.props.onAdoptPet} /></a>)}
+    
+    </div>
   }
 }
 
